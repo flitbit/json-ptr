8bb2d9f0cef1d6120634d60412a9c7b4a1b3b8ab
flitbit <phillip@flitbit.com>
2013-05-28 11:21:00 -0600

    removed speed comparisons; now in their own repo

--- examples/elapsed.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(mark, done){
-    var elapsed = process.hrtime(mark)[1] / 1000000; // divide by a million to get nano to milli
-    done(elapsed);
-    return process.hrtime();
-}
